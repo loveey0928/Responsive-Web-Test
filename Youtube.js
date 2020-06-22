cafe2aca2a69f4908ab2e6b0b96c2b90bf276efa
Joon <32371480+loveey0928@users.noreply.github.com>
2020-06-22 12:57:32 +0900

    드림코딩 Youtube 클론 완성함/ 이제 드림코딩 블로그 제작 클론 해야됨.

--- Youtube.js
+++ Youtube.js
@@ -1,7 +1,7 @@
-const detailMoreBtn = document.querySelector('.detail .detail_moreBtn');
+const clampBtn = document.querySelector('.detail .detail_moreBtn');
 const detailTitle = document.querySelector('.detail .detail_Title');
 
-detailMoreBtn.addEventListener('click', () => {
-  detailTitle.classList.toggle('fullTitled');
-  detailMoreBtn.classList.toggle('clicked');
+clampBtn.addEventListener('click', () => {
+  clampBtn.classList.toggle('clicked');
+  detailTitle.classList.toggle('clampedTitle');
 });

a58ee76ddb446482b8808947bd989c95bd244a53
Joon <32371480+loveey0928@users.noreply.github.com>
2020-08-17 21:25:39 +0900

    로그인 시 몽고DB 내의 사용자 정보와 비교하는 부분 제작 완료

---
+++ DatabaseExample/app.js
@@ -0,0 +1,192 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path'); // http 모듈과 함께 패스 문자열을 다루는 모듈
+var cookieParser = require('cookie-parser'); // 쿠키와 세션을 다룸
+var logger = require('morgan');
+var http = require('http');
+var bodyParser = require('body-parser'); // Post 요청 처리
+var expressSession = require('express-session');
+var expressErrorHandler = require('express-error-handler');
+var assert = require('assert');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var connect = require('./schemas/index');
+
+// var mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+
+//===============데이터 베이스 연결=========================
+// var database;
+// let db;
+// function connectDB() {
+//   var databaseUrl = 'mongodb://localhost:27017/'; //27017
+
+//   MongoClient.connect(
+//     databaseUrl,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     },
+//     function (err, client) {
+//       if (err) {
+//         console.log('데이터베이스 연결 에러 발생');
+//         return;
+//       }
+
+//       console.log('데이터베이스에 연결되었습니다 : ' + databaseUrl);
+
+//       database = client.db('com');
+//     }
+//   );
+// }
+//=================================================================================
+
+// var url = 'mongodb://localhost:27017/';
+
+// MongoClient.connect(
+//   'mongodb://localhost:27017',
+//   { useUnifiedTopology: true },
+//   function (err, client) {
+//     var db = client.db('shopping');
+//     assert.equal(null, err);
+//     console.log('Connected successfully to server');
+
+//     res = db.collection('users').find();
+//     res.toArray(function (err, docs) {
+//       assert.equal(err, null);
+//       console.log(docs);
+//     });
+//     client.close();
+//   }
+// );
+//=================================================================================
+
+var app = express();
+
+connect();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 3000);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/public', express.static(path.join(__dirname, 'public'))); // public 폴더 open
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // uploads 폴더 open
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.use(
+  expressSession({
+    secret: 'my key',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+var router = express.Router();
+
+router.route('/process/login').post(function (req, res) {
+  console.log('/process/login 라우팅 함수 호출됨.');
+
+  var paramId = req.body.id || req.query.id;
+  var paramPassword = req.body.password || req.query.password;
+  console.log('요청 파라미터 :' + paramId + ',' + paramPassword);
+
+  if (database) {
+    authUser(database, paramId, paramPassword, function (err, docs) {
+      if (err) {
+        console.log('에러 발생.');
+        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' });
+        res.write('<h1>에러 발생<h1>');
+        res.end();
+      }
+
+      if (docs) {
+        console.dir(docs);
+        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' });
+        res.write('<h1>사용자 로그인 성공<h1>');
+        res.write('<div><p>사용자 : ' + docs[0].name + '</p></div>');
+        res.write('<br><br><a href="/public/login.html">다시 로그인 하기</a>');
+        res.end();
+      } else {
+        console.log('에러 발생.');
+        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' });
+        res.write('<h1>사용자 데이터 조회 안됨 <h1>');
+        res.end();
+      }
+    });
+  } else {
+    console.log('에러 발생.');
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' });
+    res.write('<h1>데이터베이스 연결 안됨 </h1>');
+    res.end();
+  }
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.use('/', router);
+
+var authUser = function (db, id, password, callback) {
+  console.log('authUser 호출됨');
+
+  var users = db.collection('users');
+
+  users.find({ id: id, password: password }).toArray(function (err, docs) {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    if (docs.length > 0) {
+      console.log('일치하는 사용자를 찾음');
+      callback(null, docs);
+    } else {
+      console.log('일치하는 사용자를 찾지 못함.');
+      callback(null, null);
+    }
+  });
+};
+
+var errorHandler = expressErrorHandler({
+  static: {
+    '404': './public/404.html',
+  },
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express 서버가 ' + app.get('port') + '번 포트에서 시작됨.');
+
+  connectDB();
+});
+
+module.exports = app;
--- learn-express/app13.js
+++ learn-express/app13.js
@@ -8,13 +8,19 @@ var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
 const { runInNewContext } = require('vm');
 
+var multer = require('multer');
+var fs = require('fs');
+
+var cors = require('cors'); // 클라이언트에서 ajax로 요청 시 CORS(다중 서버 접속) 지원
+
 var app = express(); // 서버 객체 생성
 
 // app.set('port', process.env.PORT || 3000);
 
 app.use('/public', express.static(path.join(__dirname, 'public'))); // 미들웨어 사용
+app.use('/uploads', static(path.join(__dirname, 'uploads')));
 
-app.use(bodyParser.urlencoded({ extended: true })); // 미들웨어 사용
+app.use(bodyParser.urlencoded({ extended: false })); // 미들웨어 사용
 app.use(cookieParser());
 app.use(
   expressSession({
@@ -23,6 +29,29 @@ app.use(
     saveUninitialized: true,
   })
 );
+
+app.use(cors());
+
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, 'uploads'); // uploads 라는 destination 폴더 설정
+  },
+  filename: function (req, file, callback) {
+    //callback(null, fili.originalname + Date.now());
+
+    var extension = path.extname(file.originalname);
+    var baseName = path.basename(file.originalname, extension);
+    callback(null, baseName + Date.now() + extension);
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  limits: {
+    files: 10,
+    fileSize: 1024 * 1024 * 1024,
+  },
+});
 
 router.route('/process/login').post(function (req, res) {
   console.log('/process/login 라우팅 함수 호출됨.');
@@ -99,6 +128,40 @@ router.route('/process/showCookie').get(function (req, res) {
 
   res.send(req.cookies);
 });
+
+router
+  .route('/process/photo')
+  .post(upload.array('photo', 1), function (req, res) {
+    console.log('/process/photo 라우팅 함수 호출됨.');
+
+    var files = req.files;
+    console.log('=====업로드된 파일======');
+    if (files.length > 0) {
+      console.dir(files[0]);
+    } else {
+      console.log('업로드된 파일이 존재하지 않음');
+    }
+
+    var originalname;
+    var filename;
+    var mimetype;
+    var size;
+
+    if (Array.isArray(files)) {
+      for (var i = 0; i < files.length; i++) {
+        originalname = files[i].originalname;
+        filename = files[i].filename;
+        mimetype = files[i].mimetype;
+        size = files[i].size;
+      }
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf8' });
+    res.write('<h1>파일 업로드 성공</h1>');
+    res.write('<p>원본파일 : ' + originalname + '</p>');
+    res.write('<p>저장파일 : ' + filename + '</p>');
+    res.end();
+  });
 
 app.use('/', router);
 
